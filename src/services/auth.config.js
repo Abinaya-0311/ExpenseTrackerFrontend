4a4e5c423b71e462cb1dcfd88aac78953c88b6e5
Baranikumar954 <baranikumar952004@gmail.com>
2025-09-16 23:26:29 +0530

    backend url updated

--- src/services/auth.config.js
+++ src/services/auth.config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = "https://expensetrackerbackend-izox.onrender.com";
+const API_BASE_URL = "https://expensetrackerbackend-izox.onrender.com/mywallet";
 
 export default API_BASE_URL;
